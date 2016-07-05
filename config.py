bdbaa0dacc1556f4db83a02db465a4b288d2f02b
Wesley Van Pelt <wcvp@me.com>
2016-07-05 11:48:59 -0700

    Added config

---
+++ config.py
@@ -0,0 +1,20 @@
+# Max number of semantic types for a given query
+MAX_RESULTS = 5
+
+# folder where all the data added goes
+UPLOAD_FOLDER = "data/converted"
+
+DATA_FOLDER = UPLOAD_FOLDER + "/%s/data"
+MODEL_FOLDER = UPLOAD_FOLDER + "/%s/model"
+
+
+# server settings
+PORT = 5000
+DEBUG = True
+
+USER = "admin"
+PASSWORD = "secret"
+
+LOG_FILE = "error.log"
+
+ML_MODEL = "LOGISTIC"
